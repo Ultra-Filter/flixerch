ee501da956fe2452c1d7ba2fd0f6cf42757abc5f
Dor-Daniel <dor.daniel@live.biu.ac.il>
2026-08-03 11:05:19 +0300

    CREATE: created window.h\c and exposed basic window handeling functionalities and input handelings

---
+++ src/frontend/renderer/window.c
@@ -0,0 +1,403 @@
+
+#include "window.h"
+#include "glad.h"
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+static struct { 
+    GLFWwindow* handle;
+    // Screen size always non-negative, 
+    //  and it is OK to assume that the screen size is smaller then 2^32 - 1.
+    u32 width; 
+    u32 height;
+} _window;
+
+// Transitions between keys
+/*
+    up -> pressed -> down -> release -> up ...
+             ^                  ^                            
+            once               once
+*/
+typedef enum key_state {
+    KEY_STATE_UP, // key is not pressed and had not being pressed for a while
+    KEY_STATE_DOWN, // key is currently pressed
+    KEY_STATE_PRESSED, // key just pressed 
+    KEY_STATE_RELEASE, // key just released
+} _key;
+
+static struct {
+    // mouse data
+    struct {
+        u32 x, y; 
+        f64 scroll_offset;
+        _key btns [ MOUSE_BUTTON_COUNT ];
+    } mouse;
+
+    _key keys [ KEY_COUNT ];
+} _input;
+
+static struct {
+    f64 current_time;
+    f64 last_time;
+    f64 delta_time;
+} _time;
+
+static u8 key_to_index(u32 key_code)
+{
+    switch (key_code)
+    {
+        case KEY_SPACE: return 0;  
+        case KEY_APOSTROPHE: return 1;  
+        case KEY_COMMA: return 2;  
+        case KEY_MINUS: return 3;  
+        case KEY_PERIOD: return 4;  
+        case KEY_SLASH: return 5;  
+        case KEY_0: return 6;  
+        case KEY_1: return 7;  
+        case KEY_2: return 8;  
+        case KEY_3: return 9;  
+        case KEY_4: return 10;  
+        case KEY_5: return 11;  
+        case KEY_6: return 12;  
+        case KEY_7: return 13;  
+        case KEY_8: return 14;  
+        case KEY_9: return 15;  
+        case KEY_SEMICOLON: return 16;  
+        case KEY_EQUAL: return 17;  
+        case KEY_A: return 18;  
+        case KEY_B: return 19;  
+        case KEY_C: return 20;  
+        case KEY_D: return 21;  
+        case KEY_E: return 22;  
+        case KEY_F: return 23;  
+        case KEY_G: return 24;  
+        case KEY_H: return 25;  
+        case KEY_I: return 26;  
+        case KEY_J: return 27;  
+        case KEY_K: return 28;  
+        case KEY_L: return 29;  
+        case KEY_M: return 30;  
+        case KEY_N: return 31;  
+        case KEY_O: return 32;  
+        case KEY_P: return 33;  
+        case KEY_Q: return 34;  
+        case KEY_R: return 35;  
+        case KEY_S: return 36;  
+        case KEY_T: return 37;  
+        case KEY_U: return 38;  
+        case KEY_V: return 39;  
+        case KEY_W: return 40;  
+        case KEY_X: return 41;  
+        case KEY_Y: return 42;  
+        case KEY_Z: return 43;  
+        case KEY_LEFT_BRACKET: return 44;  
+        case KEY_BACKSLASH: return 45;  
+        case KEY_RIGHT_BRACKET: return 46;  
+        case KEY_GRAVE_ACCENT: return 47;  
+        case KEY_WORLD_1: return 48;  
+        case KEY_WORLD_2: return 49;  
+        case KEY_ESCAPE: return 50;  
+        case KEY_ENTER: return 51;  
+        case KEY_TAB: return 52;  
+        case KEY_BACKSPACE: return 53;  
+        case KEY_INSERT: return 54;  
+        case KEY_DELETE: return 55;  
+        case KEY_RIGHT: return 56;  
+        case KEY_LEFT: return 57;  
+        case KEY_DOWN: return 58;  
+        case KEY_UP: return 59;  
+        case KEY_PAGE_UP: return 60;  
+        case KEY_PAGE_DOWN: return 61;  
+        case KEY_HOME: return 62;  
+        case KEY_END: return 63;  
+        case KEY_CAPS_LOCK: return 64;  
+        case KEY_SCROLL_LOCK: return 65;  
+        case KEY_NUM_LOCK: return 66;  
+        case KEY_PRINT_SCREEN: return 67;  
+        case KEY_PAUSE: return 68;  
+        case KEY_F1: return 69;  
+        case KEY_F2: return 70;  
+        case KEY_F3: return 71;  
+        case KEY_F4: return 72;  
+        case KEY_F5: return 73;  
+        case KEY_F6: return 74;  
+        case KEY_F7: return 75;  
+        case KEY_F8: return 76;  
+        case KEY_F9: return 77;  
+        case KEY_F10: return 78;  
+        case KEY_F11: return 79;  
+        case KEY_F12: return 80;  
+        case KEY_F13: return 81;  
+        case KEY_F14: return 82;  
+        case KEY_F15: return 83;  
+        case KEY_F16: return 84;  
+        case KEY_F17: return 85;  
+        case KEY_F18: return 86;  
+        case KEY_F19: return 87;  
+        case KEY_F20: return 88;  
+        case KEY_F21: return 89;  
+        case KEY_F22: return 90;  
+        case KEY_F23: return 91;  
+        case KEY_F24: return 92;  
+        case KEY_F25: return 93;  
+        case KEY_KP_0: return 94;  
+        case KEY_KP_1: return 95;  
+        case KEY_KP_2: return 96;  
+        case KEY_KP_3: return 97;  
+        case KEY_KP_4: return 98;  
+        case KEY_KP_5: return 99;  
+        case KEY_KP_6: return 100;  
+        case KEY_KP_7: return 101;  
+        case KEY_KP_8: return 102;  
+        case KEY_KP_9: return 103;  
+        case KEY_KP_DECIMAL: return 104;  
+        case KEY_KP_DIVIDE: return 105;  
+        case KEY_KP_MULTIPLY: return 106;  
+        case KEY_KP_SUBTRACT: return 107;  
+        case KEY_KP_ADD: return 108;  
+        case KEY_KP_ENTER: return 109;  
+        case KEY_KP_EQUAL: return 110;  
+        case KEY_LEFT_SHIFT: return 111;  
+        case KEY_LEFT_CONTROL: return 112;  
+        case KEY_LEFT_ALT: return 113;  
+        case KEY_LEFT_SUPER: return 114;  
+        case KEY_RIGHT_SHIFT: return 115;  
+        case KEY_RIGHT_CONTROL: return 116;  
+        case KEY_RIGHT_ALT: return 117;  
+        case KEY_RIGHT_SUPER: return 118;  
+        case KEY_MENU: return 119;  
+        default:
+            UNREACHABLE("Key code does not exist");
+            return U8_MAX;
+    }
+}
+
+static u8 mouse_btn_to_index(u8 btn_code)
+{
+    if (btn_code > MOUSE_BUTTON_LAST) 
+    {
+        UNREACHABLE("Mouse button code not exists!");
+        return 0U;
+    }
+    
+    return btn_code;
+}
+
+static void  window_size_changed_callback(GLFWwindow* window, int width, int height)
+{
+    UNUSED(window);
+
+    _window.width  = (u32)width;
+    _window.height = (u32)height;
+
+    glViewport(0, 0, _window.width, _window.width);
+}
+
+static void window_key_pressed_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    UNUSED(window); UNUSED(scancode); UNUSED(mods);
+
+    u8 index = key_to_index(key);
+    _key state = action == GLFW_PRESS   ? KEY_STATE_PRESSED : 
+                 action == GLFW_REPEAT  ? KEY_STATE_DOWN    :
+                 action == GLFW_RELEASE ? KEY_STATE_RELEASE :
+                 KEY_STATE_UP;
+    if (index < KEY_COUNT) _input.keys[ index ] = state;
+} 
+
+static void window_callback_mouse_pressed(GLFWwindow* window, int button, int action, int mods)
+{
+    UNUSED(window); UNUSED(action); UNUSED(mods);
+
+    u8 index = mouse_btn_to_index(button);
+    _key state = action == GLFW_PRESS   ? KEY_STATE_PRESSED : 
+                 action == GLFW_REPEAT  ? KEY_STATE_DOWN    :
+                 action == GLFW_RELEASE ? KEY_STATE_RELEASE :
+                 KEY_STATE_UP;
+    if (index < MOUSE_BUTTON_COUNT) _input.mouse.btns[ index ] = state;
+}
+
+void window_update(void)
+{
+    _time.current_time = glfwGetTime();
+    _time.delta_time   = _time.current_time - _time.last_time;
+    _time.last_time    = _time.current_time;
+    
+    for (u8 i = 0; i < KEY_COUNT; i++)
+    {
+        _input.keys[i] = _input.keys[i] == KEY_STATE_PRESSED ? KEY_STATE_DOWN :
+        _input.keys[i] == KEY_STATE_RELEASE ? KEY_STATE_UP   :
+        _input.keys[i];
+    }
+    
+    for (u8 i = 0; i < MOUSE_BUTTON_COUNT; i++)
+    {
+        _input.mouse.btns[i] = _input.mouse.btns[i] == KEY_STATE_PRESSED ? KEY_STATE_DOWN :
+        _input.mouse.btns[i] == KEY_STATE_RELEASE ? KEY_STATE_UP   :
+        _input.mouse.btns[i]; 
+    }
+
+    glfwPollEvents();
+    glfwSwapBuffers(_window.handle); 
+}
+
+bool window_init(u32 width, u32 height, const char* title)
+{
+    // Init GLFW and glfw-window
+    if (!glfwInit())
+    {
+        LOG_ERROR("EROR: initializing GLFW.\n");
+        return false;
+    }
+
+    _window.handle = glfwCreateWindow((int)width, (int)height, title, NULL, NULL);
+    
+    if (!_window.handle)
+    {
+        LOG_ERROR("EROR: initializing glfw-window.\n");
+        return false;
+    }
+
+    _window.width  = width;
+    _window.height = height;
+    
+    // TODO: add required hints
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+    glfwMaximizeWindow(_window.handle); // ??
+    
+    // TODO: set callbacks
+    glfwSetFramebufferSizeCallback(_window.handle, window_size_changed_callback);
+    glfwSetKeyCallback(_window.handle, window_key_pressed_callback);
+    glfwSetMouseButtonCallback(_window.handle, window_callback_mouse_pressed);
+
+
+    // Init openGL
+    glfwMakeContextCurrent(_window.handle);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fprintf(stderr, "ERROR: init glad");
+        return false;
+    }
+    
+   
+    // TODO: glEnable(stuff)...
+    glViewport(0,0,_window.width, _window.height);
+    
+    return true;
+}
+
+bool window_should_close(void)
+{
+    return glfwWindowShouldClose(_window.handle);
+}
+
+bool is_key_pressed(u32 key_code)
+{
+    u8 index = key_to_index(key_code);
+    if (index <= KEY_COUNT)
+        return _input.keys[ index ] == KEY_STATE_PRESSED;
+    return false;
+}
+
+bool is_key_released(u32 key_code)
+{
+    u8 index = key_to_index(key_code);
+    if (index <= KEY_COUNT)
+        return _input.keys[ index ] == KEY_STATE_RELEASE;
+    return false;
+}
+
+bool is_key_down(u32 key_code)
+{
+    u8 index = key_to_index(key_code);
+    if (index <= KEY_COUNT)
+        return _input.keys[ index ] == KEY_STATE_DOWN;
+    return false;
+}
+
+bool is_key_up(u32 key_code)
+{
+    u8 index = key_to_index(key_code);
+    if (index <= KEY_COUNT)
+        return _input.keys[ index ] == KEY_STATE_UP;
+    return false;
+}
+
+bool is_mouse_button_pressed(u8 button_code)
+{
+    u8 index = mouse_btn_to_index(button_code);
+    if (index <= KEY_COUNT)
+        return _input.mouse.btns[ index ] == KEY_STATE_PRESSED;
+    return false;
+}
+
+bool is_mouse_button_released(u8 button_code)
+{
+    u8 index = mouse_btn_to_index(button_code);
+    if (index <= KEY_COUNT)
+        return _input.mouse.btns[ index ] == KEY_STATE_RELEASE;
+    return false;
+}
+
+bool is_mouse_button_down(u8 button_code)
+{
+    u8 index = mouse_btn_to_index(button_code);
+    if (index <= KEY_COUNT)
+        return _input.mouse.btns[ index ] == KEY_STATE_DOWN;
+    return false;
+}
+
+bool is_mouse_button_up(u8 button_code)
+{
+    u8 index = mouse_btn_to_index(button_code);
+    if (index <= KEY_COUNT)
+        return _input.mouse.btns[ index ] == KEY_STATE_UP;
+    return false;
+}
+
+void window_destroy(void)
+{
+    glfwDestroyWindow(_window.handle);
+    glfwTerminate();
+}
+
+void window_clear_screen(void)
+{
+    glClearColor(1.0F, 1.0F, 1.0F, 1.0F);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+u32 get_mouse_x(void)
+{
+    return _input.mouse.x;
+}
+
+u32 get_mouse_y(void)
+{
+    return _input.mouse.x;
+}
+
+u32 get_window_width(void)
+{
+    return _window.width;
+}
+
+u32 get_window_height(void)
+{
+    return _window.height;
+}
+
+f64 get_mouse_scroll_offset(void)
+{
+    return _input.mouse.scroll_offset;
+}
+
+f64 get_time(void)
+{
+    return _time.current_time;
+}
+
+f64 get_delta_time(void)
+{
+    return _time.delta_time;
+}
---
+++ src/frontend/renderer/window.h
@@ -0,0 +1,158 @@
+#pragma once
+#include "defines.h"
+
+#define KEY_SPACE              32U
+#define KEY_APOSTROPHE         39U  /* ' */
+#define KEY_COMMA              44U  /* , */
+#define KEY_MINUS              45U  /* - */
+#define KEY_PERIOD             46U  /* . */
+#define KEY_SLASH              47U  /* / */
+#define KEY_0                  48U
+#define KEY_1                  49U
+#define KEY_2                  50U
+#define KEY_3                  51U
+#define KEY_4                  52U
+#define KEY_5                  53U
+#define KEY_6                  54U
+#define KEY_7                  55U
+#define KEY_8                  56U
+#define KEY_9                  57U
+#define KEY_SEMICOLON          59U  /* ; */
+#define KEY_EQUAL              61U  /* = */
+#define KEY_A                  65U
+#define KEY_B                  66U
+#define KEY_C                  67U
+#define KEY_D                  68U
+#define KEY_E                  69U
+#define KEY_F                  70U
+#define KEY_G                  71U
+#define KEY_H                  72U
+#define KEY_I                  73U
+#define KEY_J                  74U
+#define KEY_K                  75U
+#define KEY_L                  76U
+#define KEY_M                  77U
+#define KEY_N                  78U
+#define KEY_O                  79U
+#define KEY_P                  80U
+#define KEY_Q                  81U
+#define KEY_R                  82U
+#define KEY_S                  83U
+#define KEY_T                  84U
+#define KEY_U                  85U
+#define KEY_V                  86U
+#define KEY_W                  87U
+#define KEY_X                  88U
+#define KEY_Y                  89U
+#define KEY_Z                  90U
+#define KEY_LEFT_BRACKET       91U  /* [ */
+#define KEY_BACKSLASH          92U  /* \ */
+#define KEY_RIGHT_BRACKET      93U  /* ] */
+#define KEY_GRAVE_ACCENT       96U  /* ` */
+#define KEY_WORLD_1            161U /* non-US #1 */
+#define KEY_WORLD_2            162U /* non-US #2 */
+#define KEY_ESCAPE             256U
+#define KEY_ENTER              257U
+#define KEY_TAB                258U
+#define KEY_BACKSPACE          259U
+#define KEY_INSERT             260U
+#define KEY_DELETE             261U
+#define KEY_RIGHT              262U
+#define KEY_LEFT               263U
+#define KEY_DOWN               264U
+#define KEY_UP                 265U
+#define KEY_PAGE_UP            266U
+#define KEY_PAGE_DOWN          267U
+#define KEY_HOME               268U
+#define KEY_END                269U
+#define KEY_CAPS_LOCK          280U
+#define KEY_SCROLL_LOCK        281U
+#define KEY_NUM_LOCK           282U
+#define KEY_PRINT_SCREEN       283U
+#define KEY_PAUSE              284U
+#define KEY_F1                 290U
+#define KEY_F2                 291U
+#define KEY_F3                 292U
+#define KEY_F4                 293U
+#define KEY_F5                 294U
+#define KEY_F6                 295U
+#define KEY_F7                 296U
+#define KEY_F8                 297U
+#define KEY_F9                 298U
+#define KEY_F10                299U
+#define KEY_F11                300U
+#define KEY_F12                301U
+#define KEY_F13                302U
+#define KEY_F14                303U
+#define KEY_F15                304U
+#define KEY_F16                305U
+#define KEY_F17                306U
+#define KEY_F18                307U
+#define KEY_F19                308U
+#define KEY_F20                309U
+#define KEY_F21                310U
+#define KEY_F22                311U
+#define KEY_F23                312U
+#define KEY_F24                313U
+#define KEY_F25                314U
+#define KEY_KP_0               320U
+#define KEY_KP_1               321U
+#define KEY_KP_2               322U
+#define KEY_KP_3               323U
+#define KEY_KP_4               324U
+#define KEY_KP_5               325U
+#define KEY_KP_6               326U
+#define KEY_KP_7               327U
+#define KEY_KP_8               328U
+#define KEY_KP_9               329U
+#define KEY_KP_DECIMAL         330U
+#define KEY_KP_DIVIDE          331U
+#define KEY_KP_MULTIPLY        332U
+#define KEY_KP_SUBTRACT        333U
+#define KEY_KP_ADD             334U
+#define KEY_KP_ENTER           335U
+#define KEY_KP_EQUAL           336U
+#define KEY_LEFT_SHIFT         340U
+#define KEY_LEFT_CONTROL       341U
+#define KEY_LEFT_ALT           342U
+#define KEY_LEFT_SUPER         343U
+#define KEY_RIGHT_SHIFT        344U
+#define KEY_RIGHT_CONTROL      345U
+#define KEY_RIGHT_ALT          346U
+#define KEY_RIGHT_SUPER        347U
+#define KEY_MENU               348U
+#define KEY_LAST               KEY_MENU
+#define KEY_COUNT              121U
+
+#define MOUSE_BUTTON_LEFT      0
+#define MOUSE_BUTTON_RIGHT     1
+#define MOUSE_BUTTON_MIDDLE    2
+#define MOUSE_BUTTON_4         3
+#define MOUSE_BUTTON_5         4
+#define MOUSE_BUTTON_6         5
+#define MOUSE_BUTTON_7         6
+#define MOUSE_BUTTON_8         7
+#define MOUSE_BUTTON_LAST      MOUSE_BUTTON_8
+#define MOUSE_BUTTON_COUNT     8
+
+
+bool window_init              (u32 width, u32 height, const char * title);
+bool window_should_close      (void);
+bool is_key_pressed           (u32 key_code);
+bool is_key_released          (u32 key_code);
+bool is_key_down              (u32 key_code);
+bool is_key_up                (u32 key_code);
+bool is_mouse_button_pressed  (u8 button_code);
+bool is_mouse_button_released (u8 button_code);
+bool is_mouse_button_down     (u8 button_code);
+bool is_mouse_button_up       (u8 button_code);
+void window_update            (void); // Update window last in main loop
+void window_destroy           (void);
+void window_clear_screen      (void);
+u32  get_mouse_x              (void);
+u32  get_mouse_y              (void);
+u32  get_window_width         (void);
+u32  get_window_height        (void);
+f64  get_mouse_scroll_offset  (void);
+f64  get_time                 (void);
+f64  get_delta_time           (void);
